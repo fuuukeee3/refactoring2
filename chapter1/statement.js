7d5d2158ae5d38ad1b971fefd5a24272479240d7
fuuukeee3 <semarusika@gmail.com>
2020-08-12 23:17:23 +0900

    ボリューム特典ポイント集計箇所の修正

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -1,19 +1,18 @@
 export default function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
+
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-
     // 注文の内訳を出力
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
+
   result += `Amount owed is ${usd(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 
   // --------------------
@@ -60,5 +59,13 @@ export default function statement(invoice, plays) {
        currency: "USD",
        minimumFractionDigits: 2,
      }).format(aNumber / 100);
+  }
+
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
   }
 }
