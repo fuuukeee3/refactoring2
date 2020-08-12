61f9490411c81e61f12457c873803e2563ff3a89
fuuukeee3 <semarusika@gmail.com>
2020-08-12 23:11:56 +0900

    format変数の削除

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -3,22 +3,16 @@ export default function statement(invoice, plays) {
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
-
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf)
+    volumeCredits += volumeCreditsFor(perf);
 
     // 注文の内訳を出力
-    result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
@@ -58,5 +52,13 @@ export default function statement(invoice, plays) {
     result += Math.max(aPerformance.audience - 30, 0);
     if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
     return result;
+  }
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+       style: "currency",
+       currency: "USD",
+       minimumFractionDigits: 2,
+     }).format(aNumber / 100);
   }
 }
